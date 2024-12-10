12563554ee6a90693e087ba76b2083ddbdeedd88
PhuongThao00000 <162420678+PhuongThao00000@users.noreply.github.com>
2024-12-10 12:03:40 +0700

    Update LINKLIST.h

--- LINKLIST.h
+++ LINKLIST.h
@@ -5,34 +5,33 @@
 using namespace std;
 
 template <typename T>
-struct Node{
+struct Node {
     T data;
-    Node<T> *next;
-    Node<T> *prev;
-    Node(T value):data(value),prev(nullptr),next(nullptr){}
+    Node<T>* next;
+    Node<T>* prev;
+    Node(T value) : data(value), prev(nullptr), next(nullptr) {}
 };
 
 template<typename T>
 class LINKLIST {
-    private:
-        Node<T>* head;
-        Node<T>* tail;//thêm con trỏ tail để tối ưu hóa
-        int size;
-    public:
-        LINKLIST();
-        ~LINKLIST();
-        void addFirst(T const data);
-        void addLast(T const data);
-        Node<T>* begin() const;
-        Node<T>* end() const;
-        Node<T>* find(T const data) const;
-        //Node<T>* operator[](int index) const;
-        void removeFirst();
-        void removeLast();
-        //void removeMiddle(int pos);
-        void remove(Node<T>* node);
-        void display()const;
-        int getSize() const {return size;}
+private:
+    Node<T>* head;
+    Node<T>* tail;
+    int size;
+
+public:
+    LINKLIST();
+    ~LINKLIST();
+    void addFirst(T const data);
+    void addLast(T const data);
+    Node<T>* begin() const;
+    Node<T>* end() const;
+    Node<T>* find(T const data) const;
+    void removeFirst();
+    void removeLast();
+    void remove(Node<T>* node);
+    void display() const;
+    int getSize() const { return size; }
 };
 
 #endif
