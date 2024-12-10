2570f4caf185199b802f966e5659c2f74cf12003
PhuongThao00000 <162420678+PhuongThao00000@users.noreply.github.com>
2024-12-10 11:48:35 +0700

    Update KHACHHANG.h

--- KHACHHANG.h
+++ KHACHHANG.h
@@ -2,43 +2,36 @@
 #define KHACHHANG_H
 
 #include "USER.h"
-#include "USER.cpp"
 #include "QuanLyDatSan.h"
-#include "QuanLyDatSan.cpp"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class KhachHang : public User { 
-    int rankMember=0;//4: Member,3: Dong, 2: Bac, 1: Vang
+class KhachHang : public User {
+private:
+    int rankMember = 0; // 4: Thanh Vien, 3: Dong, 2: Bac, 1: Vang
+
 public:
-    KhachHang();  // Constructor 
-    KhachHang(const string& username , const string& password ,const string& phonenumber,int gender, Date& ngsinh,int rankmember=0,int loaiTK=0); 
-    KhachHang(const KhachHang *kh);
-    ~KhachHang(){}  // Destructor
-    void setRank(int rank){
-        if(rank!=1&&rank!=2&&rank!=3&&rank!=4)
-            throw invalid_argument("Hạng thành viên không hợp lệ!");
-        rankMember=rank;
-    }
-    const int& getRank(){
-        return rankMember;
-    }
-    void xuatthongtin() const{
-        User::xuatthongtin();
-        cout<<"Hạng thành viên(4:Member,3:Đồng,2:Bạc,1:Vàng): ";
-        if(rankMember==0) cout << "Chưa có hạng\n";
-        else{ cout<< rankMember;}
-    };
-    void themDonDatsanLe(QuanLyDatSan&qlds);
-    
-    void xoaSan(QuanLyDatSan&qlds);
-    DatSan* timkiem(QuanLyDatSan&qlds);
-    void suaDonDatsan(QuanLyDatSan&qlds);
-    void xuatDonDatsan(QuanLyDatSan&qlds);
-    // friend ostream& operator <<(ostream& os, const KhachHang& kh);
+    // Constructor
+    KhachHang();  // Constructor mac dinh
+    KhachHang(const string& username, const string& password, const string& phonenumber, int gender, Date& ngsinh, int rankmember = 0, int loaiTK = 0); 
+    KhachHang(const KhachHang *kh);  // Constructor copy
+    ~KhachHang();  // Destructor
+
+    // Getter, Setter cho rankMember
+    void setRank(int rank);
+    const int& getRank();
+
+    // Xuat thong tin KhachHang
+    void xuatthongtin() const;
+
+    // Phuong thuc them, sua, xoa, tim kiem don dat san
+    void themDonDatsanLe(QuanLyDatSan& qlds);
+    void xoaSan(QuanLyDatSan& qlds);
+    DatSan* timkiem(QuanLyDatSan& qlds);
+    void suaDonDatsan(QuanLyDatSan& qlds);
+    void xuatDonDatsan(QuanLyDatSan& qlds);
 };
 
-        
-#endif
+#endif // KHACHHANG_H
