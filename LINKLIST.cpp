86d492d4d132019e3914b15bceaa10d294e8e463
PhuongThao00000 <162420678+PhuongThao00000@users.noreply.github.com>
2024-12-10 12:04:00 +0700

    Update LINKLIST.cpp

--- LINKLIST.cpp
+++ LINKLIST.cpp
@@ -1,10 +1,9 @@
 #include "LINKLIST.h"
 
 template<typename T>
-LINKLIST<T>::LINKLIST():head(nullptr),tail(nullptr),size(0){}   
+LINKLIST<T>::LINKLIST() : head(nullptr), tail(nullptr), size(0) {}
 
-
-template <typename T>
+template<typename T>
 LINKLIST<T>::~LINKLIST() {
     Node<T>* current = head;
     while (current) {
@@ -16,25 +15,23 @@ LINKLIST<T>::~LINKLIST() {
 
 template <typename T>
 void LINKLIST<T>::addFirst(const T data) {
-    Node<T>* newFirst = new Node<T>(data);//cấp phát động
-    if(!head){
+    Node<T>* newFirst = new Node<T>(data);
+    if (!head) {
         head = tail = newFirst;
-    }
-    else{
-    newFirst->next = head;
-    head->prev = newFirst;
-    head = newFirst;//cập nhật con trỏ head
+    } else {
+        newFirst->next = head;
+        head->prev = newFirst;
+        head = newFirst;
     }
     size++;
 }
 
 template <typename T>
-void LINKLIST<T>::addLast(const T data){
+void LINKLIST<T>::addLast(const T data) {
     Node<T>* newLast = new Node<T>(data);
-    if(!tail){
+    if (!tail) {
         head = tail = newLast;
-    }
-    else{
+    } else {
         newLast->prev = tail;
         tail->next = newLast;
         tail = newLast;
@@ -43,70 +40,66 @@ void LINKLIST<T>::addLast(const T data){
 }
 
 template <typename T>
-Node<T>* LINKLIST<T>::begin() const{
+Node<T>* LINKLIST<T>::begin() const {
     return head;
 }
 
 template <typename T>
-Node<T>* LINKLIST<T>::end() const{
+Node<T>* LINKLIST<T>::end() const {
     return tail;
 }
 
 template <typename T>
-void LINKLIST<T>::removeFirst(){
-    if(!head) return;
-    Node<T>* temp = head;//Node cần xóa
-    head = head->next;//head của list sau khi xóa
-    if(head){
-        head->prev=nullptr;
-    }
-    else{
-        tail = nullptr;//Danh sách rỗng
+void LINKLIST<T>::removeFirst() {
+    if (!head) return;
+    Node<T>* temp = head;
+    head = head->next;
+    if (head) {
+        head->prev = nullptr;
+    } else {
+        tail = nullptr;
     }
     delete temp;
     size--;
 }
 
 template <typename T>
-void LINKLIST<T>::removeLast(){
-    if(!tail)return;
+void LINKLIST<T>::removeLast() {
+    if (!tail) return;
     Node<T>* temp = tail;
     tail = tail->prev;
-    if(tail){//Điều kiện tail khác rỗng
+    if (tail) {
         tail->next = nullptr;
-    }
-    else{
-        head = nullptr;//Danh sách rỗng
+    } else {
+        head = nullptr;
     }
     delete temp;
     size--;
 }
 
 template <typename T>
-void LINKLIST<T>::remove(Node<T>* node){
-    if(!head||!node){
-    cout << "Danh sách rỗng hoặc không hợp lệ!\n";
-    return;
+void LINKLIST<T>::remove(Node<T>* node) {
+    if (!head || !node) {
+        cout << "Danh sach rong hoac khong hop le!\n";
+        return;
     }
-    if(node==head){
+    if (node == head) {
         removeFirst();
-    }
-    else if(node==tail){
+    } else if (node == tail) {
         removeLast();
-    }
-    else{
-        node->prev->next=node->next;
-        node->next->prev=node->prev;
+    } else {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
         delete node;
         size--;
     }
 }
 
 template <typename T>
-Node<T>* LINKLIST<T>::find(const T data) const{
+Node<T>* LINKLIST<T>::find(const T data) const {
     Node<T>* current = head;
-    while(current){
-        if(*current->data==*data){
+    while (current) {
+        if (*current->data == *data) {
             return current;
         }
         current = current->next;
@@ -114,17 +107,12 @@ Node<T>* LINKLIST<T>::find(const T data) const{
     return nullptr;
 }
 
-
 template <typename T>
-void LINKLIST<T>::display() const{
-    //head = head ->next;
+void LINKLIST<T>::display() const {
     Node<T>* current = head;
-    while(current){
-        cout  << *current->data;
-        current=current->next;
+    while (current) {
+        cout << *current->data;
+        current = current->next;
     }
     cout << endl;
 }
-
-
-
