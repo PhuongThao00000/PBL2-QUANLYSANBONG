bbf0ee794d5ba4474b0090e77b26540204462413
PhuongThao00000 <162420678+PhuongThao00000@users.noreply.github.com>
2024-12-10 11:57:11 +0700

    Update NHANVIEN.h

--- NHANVIEN.h
+++ NHANVIEN.h
@@ -1,76 +1,34 @@
 #ifndef NHANVIEN_H
 #define NHANVIEN_H
 
-#include "QuanLyDatNuoc.h"
-#include "QuanLyDatNuoc.cpp"
 #include "USER.h"
-// #include "QuanLyDatSan.h"
-// #include "QuanLyDatSan.cpp"
+#include "QuanLyDatNuoc.h"
 #include "QuanLyDatXe.h"
-#include "QuanLyDatXe.cpp"
-// #include "QuanLyHoaDon.h"
-// #include "QuanLyHoaDon.cpp"
-// #include "QuanLyKH.h"
-// #include "QuanLyKH.cpp"
+#include <iostream>
+#include <string>
 
 class NhanVien : public User {
-
 public:
-    NhanVien():User(){}
-    NhanVien(const string& username , const string& password ,const string& phonenumber,int gender, Date& ngsinh,int loaitk=1)
-        : User(username,password,phonenumber,gender,ngsinh,loaitk){}
+    NhanVien();  // Constructor mac dinh
+    NhanVien(const string& username, const string& password, const string& phonenumber, int gender, Date& ngsinh, int loaitk = 1); 
+    ~NhanVien();  // Destructor
 
-    ~NhanVien() {}
+    // Xuat thong tin NhanVien
+    void xuatthongtin() const;
 
-    void xuatthongtin()const;
-    // Quản lý khách hàng
-    // void themKhachHang(KhachHang* kh);
+    // Quản lý đặt nước
+    void themDatNuoc(QuanLyDatNuoc& qldn);
+    DatNuoc* timkiemDatNuoc(QuanLyDatNuoc& qldn, const int& maDon);
+    void capnhatDatNuoc(QuanLyDatNuoc& qldn, const int& maDon);
+    void xoaDatNuoc(QuanLyDatNuoc& qldn, const int& maDon);
+    void xuatDatNuocTheoNgay(QuanLyDatNuoc& qldn, int day, int month, int year);
 
-    // void xoaKhachHang(string sdt);
-
-    // void capNhatKhachHang(string sdt) ;
-    // void timKiemKhachHang(string sdt);
-
-    // Quản lý đặt sân
-    // void themDatSanLe(string sdt) ;
-
-    // void themDatSanDinhKy(string sdt) ;
-
-    // void xoaDatSan(string sdt) ;
-
-    // void capNhatDatSan(string sdt) ;
-
-    // Quản lý hóa đơn
-    // void themHoaDon(HoaDon* hd) ;
-
-    // void xoaHoaDon(string sdt) ;
-
-    // void capNhatHoaDon(string sdt, string status) ;
-
-    // void thongKeDoanhThu(string ngay) ;
-
-    // Quản lý đặt nước(co ngay va gio ban)
-    void themDatNuoc(QuanLyDatNuoc&qldn) ;
-
-    DatNuoc* timkiemDatNuoc(QuanLyDatNuoc&qldn, const int& maDon);
-
-    void capnhatDatNuoc(QuanLyDatNuoc&qldn, const int& maDon);
-
-    void xoaDatNuoc(QuanLyDatNuoc&qldn,const int& maDon);
-
-    void xuatDatNuocTheoNgay(QuanLyDatNuoc&qldn,int day, int month, int year);
-    //Quan ly cac ve giu xe(co ngay va gio giu xe)
-    void themDatXe(QuanLyDatXe&qldx) ;
-
-    DatXe* timkiemDatXe(QuanLyDatXe&qldx, const int& maDon);
-
-    void capnhatGiuXe(QuanLyDatXe&qldx,const int& maDon);
-
-    void xoaGiuXe(QuanLyDatXe&qldx,const int& maDon);
-
-    void xuatGiuXeTheoNgay(QuanLyDatXe&qldx,int day, int month, int year);
-
+    // Quản lý giữ xe
+    void themDatXe(QuanLyDatXe& qldx);
+    DatXe* timkiemDatXe(QuanLyDatXe& qldx, const int& maDon);
+    void capnhatGiuXe(QuanLyDatXe& qldx, const int& maDon);
+    void xoaGiuXe(QuanLyDatXe& qldx, const int& maDon);
+    void xuatGiuXeTheoNgay(QuanLyDatXe& qldx, int day, int month, int year);
 };
 
-
-#endif
+#endif // NHANVIEN_H
